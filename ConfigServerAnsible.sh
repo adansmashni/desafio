20c2fe449c15396de6aaeb249dec33a5c3fad471
Adans Mashni <adans.mashni@gmail.com>
2020-02-01 14:39:41 -0300

    Continuando teste ansible

--- ConfigServerAnsible.sh
+++ ConfigServerAnsible.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Instala Python
-sudo yum install -y python3
-sudo pip3 install ansible
+sudo yum install -y git epel-release
+sudo yum install -y python2-pip
 
 # Aplica ansible playbook
 ansible-playbook --become --connection=local -i 127.0.0.1 ConfiguraServer.yml
