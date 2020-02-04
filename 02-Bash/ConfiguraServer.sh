509897f1fbcb0ebe0fef8564296030f6b572f0e9
Adans Kmal Falcao Mashni <adans.mashni@meta.com.br>
2020-02-04 15:49:59 -0200

    adicionando steps na configuração do servidor

--- 02-Bash/ConfiguraServer.sh
+++ 02-Bash/ConfiguraServer.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 # Instala Pacotes Iniciais
-sudo yum install - y vim bash-completion yum-utils wget curl net-tools bind-utils git telnet python3
+sudo yum install - y vim bash-completion yum-utils wget curl git net-tools bind-utils git telnet python3 gcc-c++ make automake
+
+# Configura repositorio nodejs
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+
+# instala nodejs
+yum install -y nodejs
+
+# cria usuário node
+adduser node
+
+# configura ambiente para usuario node
+
+
+
 
 
 
